afc4d03bbad4df46f658e05c7540d52283a457e8
Mateus <mateusc.ramos@outlook.com>
2024-11-19 00:35:47 -0300

    feat: add auth routes blueprint

---
+++ backpack/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, Blueprint, jsonify
+from backpack.routes import auth
+
+app = Flask(__name__)
+
+api = Blueprint("api", __name__, url_prefix="/api")
+
+api.register_blueprint(auth.bp)
+
+@api.route("/")
+def index():
+    return "Hello, world!"
+
+app.register_blueprint(api)
