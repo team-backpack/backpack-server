9c53083b82acf8d08f093a9b0b0cd303c0092bbf
Mateus <mateusc.ramos@outlook.com>
2024-11-24 17:14:47 -0300

    feat: add send verification token function

--- backpack/utils/emailing.py
+++ backpack/utils/emailing.py
@@ -2,22 +2,29 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from config import EMAIL_PASSWORD
-from flask import render_template
 
 port = 465
 smtp_server = "smtp.gmail.com"
 from_email = "social.backpack.bp@gmail.com"
 password = EMAIL_PASSWORD
 
-def send(to_email: str, subject: str, body: str):
+def send(to: str = "", subject: str = "", body: str = ""):
 
     message = MIMEMultipart()
     message["From"] = from_email
-    message["To"] = to_email
+    message["To"] = to
     message["Subject"] = subject
     message.attach(MIMEText(body, "html"))
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(from_email, password)
-        server.sendmail(from_email, to_email, message.as_string())
+        server.sendmail(from_email, to, message.as_string())
+
+
+def send_token(email: str, token: str = ""):
+    with open("backpack/templates/token.html", mode="r", encoding="UTF-8") as file:
+        body = file.read()
+        body = body.format(token)
+
+        send(to=email, subject="Seja bem-vindo ao Backpack, aventureiro!", body=body)
