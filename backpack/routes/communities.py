f97f98b8f924d24fc567b1e3fc6dcbba4f2d3e83
mat-afk <mateusc.ramos@outlook.com>
2024-12-11 17:39:09 -0300

    feat: add community participant management routes

--- backpack/routes/communities.py
+++ backpack/routes/communities.py
@@ -145,7 +145,7 @@ def participants(community_id: str):
             user_id = jwt.get_current_user_id(request.cookies.get("jwt"))
 
             if not Participant.find_one(community_id=community_id, user_id=user_id):
-                return jsonify({ "error": "User is not in community" }), 400
+                return jsonify({ "error": "User not in community" }), 400
 
             if Participant.find_all(limit=2) == 1:
                 Community.delete(id=community_id)
@@ -153,6 +153,65 @@ def participants(community_id: str):
             Participant.delete(user_id=user_id, community_id=community_id)
 
             return jsonify({ "message": "Exited community successfully" }), 201
+        
+    except Exception as e:
+        print(e)
+        return jsonify({ "error": "Internal Server Error" }), 500
+    
+
+@bp.route("/<string:community_id>/participants/<string:participant_id>/", methods=["PATCH", "DELETE"])
+def participants(community_id: str, participant_id: str):
+    try:
+        user_id = jwt.get_current_user_id(request.cookies.get("jwt"))
+
+        if request.method == "PATCH":
+            data = request.get_json()
+
+            role = data.get("role", None)
+            is_suspended = data.get("isSuspended", None)
+
+            current_user = Participant.find_one(user_id=user_id, community_id=community_id)
+            if not current_user:
+                return jsonify({ "error": "User not in community" }), 404            
+
+            participant = Participant.find_one(id=participant_id)
+            if not participant:
+                return jsonify({ "error": "Participant not in community" }), 404
+            
+            if role:
+                can_update_role = current_user.role == "admin"
+                if not can_update_role:
+                    return jsonify({ "error": "Do not have permission to modify participants' roles" }), 401
+                
+                participant.role = role if role in ("admin", "moderators", "member") else participant.role
+
+            if is_suspended:
+                can_suspend = current_user.role in ("moderator", "admin")
+                if not can_suspend:
+                    return jsonify({ "error": "Do not have permission to suspend participants" }), 401
+                
+                participant.is_suspended = is_suspended
+
+            participant.update()
+
+            return jsonify(participant.to_dict()), 200
+
+        if request.method == "DELETE":
+            current_user = Participant.find_one(user_id=user_id, community_id=community_id)
+            if not current_user:
+                return jsonify({ "error": "User not in community" }), 404            
+
+            participant = Participant.find_one(id=participant_id)
+            if not participant:
+                return jsonify({ "error": "Participant not in community" }), 404
+            
+            can_throw_out = current_user.role in ("admin", "moderator")
+            if not can_throw_out:
+                return jsonify({ "error": "Do not have permission to throw out participants" }), 401
+            
+            Participant.delete(id=participant_id)
+
+            return jsonify({ "message": "Participant thrown out successfully" }), 201
         
     except Exception as e:
         print(e)
